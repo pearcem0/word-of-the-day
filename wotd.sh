adbea47da79022ce2590dec26417184d2deb4e18
Michael Pearce <michael@rootofpi.co.uk>
2017-09-19 00:07:05 +0100

    clean up format and output
    * Shortened line length
    * Added another html tag to remove

--- wotd.sh
+++ wotd.sh
@@ -4,30 +4,40 @@ while getopts ":d:" opt; do
   case $opt in
     d)
       selecteddate=$OPTARG
-      wordoftheday=`curl -s -L "www.dictionary.com/wordoftheday/$selecteddate" | grep "<title>" | sed 's/<title>//g' | cut -d "-" -f 1 | sed 's: | Dictionary.com</title>::g' | tr  -d ' '`
+      wordoftheday=`curl -s -L "www.dictionary.com/wordoftheday/$selecteddate" \
+      | grep "<title>" | sed 's/<title>//g' | cut -d "-" -f 1 | sed 's: | \
+      Dictionary.com</title>::g' | tr  -d ' '`
       ;;
   esac
 done
 
-[ -z "$selecteddate" ] && selecteddate=`date +%Y/%m/%d` && wordoftheday=`curl -s -L "www.dictionary.com/wordoftheday/$selecteddate" | grep "<title>" | sed 's/<title>//g' | cut -d "-" -f 2 | sed 's: | Dictionary.com</title>::g' | tr  -d ' '`
+[ -z "$selecteddate" ] && selecteddate=`date +%Y/%m/%d` && wordoftheday=`curl \
+-s -L "www.dictionary.com/wordoftheday/$selecteddate" | grep "<title>" | \
+sed 's/<title>//g' | cut -d "-" -f 2 | sed 's: | Dictionary.com</title>::g' \
+| tr  -d ' '`
 
-# on the same day, and before the current days wotd has not yet been added a different format is used, so get the next field in the cut
+# on the same day, and before the current days wotd has not yet been added a
+# different format is used, so get the next field in the cut
 if [ "$wordoftheday" = "GettheWordoftheDay" ]; then
-  wordoftheday=`curl -s -L "www.dictionary.com/wordoftheday/$selecteddate" | grep "<title>" | sed 's/<title>//g' | cut -d "-" -f 2 | sed 's: | Dictionary.com</title>::g' | tr  -d ' '`
+  wordoftheday=`curl -s -L "www.dictionary.com/wordoftheday/$selecteddate" | \
+  grep "<title>" | sed 's/<title>//g' | cut -d "-" -f 2 | sed 's: \
+  | Dictionary.com</title>::g' | tr  -d ' '`
 fi
 
 printf "Today's Word of the Day is... $wordoftheday!\n\n"
 
 # Get a page to work with and manipulate
 
-curl -sL "www.dictionary.com/wordoftheday/$selecteddate/$wordoftheday" > wotd.page
+curl -sL "www.dictionary.com/wordoftheday/$selecteddate/$wordoftheday" \
+> wotd.page
 
 # Get some stats
 # linecount
 linecount=`sed -n '$=' wotd.page`
 
-# Definition starts on line - 
-definitionStart=`cat -n wotd.page | grep '<ol class="definition-list'  | cut -d " " -f 4`
+# Definition starts on line -
+definitionStart=`cat -n wotd.page | grep '<ol class="definition-list'  | \
+cut -d " " -f 4`
 
 tail -n $(($linecount - $definitionStart)) wotd.page > wotd.page.top
 
@@ -37,9 +47,12 @@ head -n $(($definitionEnd-1)) wotd.page.top > wotd.page.finalcut
 
 cat wotd.page.finalcut | while read line
 do
-  echo $line | awk -F"<span>" '{print $2}' | awk -F"</span>" '{print $1}' | sed -e 's:<em>::g' -e 's:</em>::g'
+  echo $line | awk -F"<span>" '{print $2}' | awk -F"</span>" '{print $1}' \
+  | sed -e 's:<em>::g' -e 's:</em>::g' | \
+  sed -e 's:<strong>::g' -e 's:</strong>::g'
 done
 
-printf "\nTo read more you can go to https://www.dictionary.com/browse/$wordoftheday\n\n"
+printf "\nTo read more you can go \
+to https://www.dictionary.com/browse/$wordoftheday\n\n"
 
 rm wotd.page*
