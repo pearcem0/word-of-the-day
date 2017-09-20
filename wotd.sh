b46504d4e3b683d6ec1fb68e8bcef33476bcb854
Michael Pearce <michael@rootofpi.co.uk>
2017-09-20 13:06:22 +0100

    Added checks for future date * if date selected is greater than the current date throw an error and exit.

--- wotd.sh
+++ wotd.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
+function exitscript {
+  exit 1;
+}
 
 while getopts ":d:" opt; do
   case $opt in
     d)
       selecteddate=$OPTARG
-      wordoftheday=`curl -s -L "www.dictionary.com/wordoftheday/$selecteddate" \
-      | grep "<title>" | sed 's/<title>//g' | cut -d "-" -f 1 | sed 's: | \
-      Dictionary.com</title>::g' | tr  -d ' '`
       ;;
   esac
 done
+
+if [ "$selecteddate" ];
+then
+  correctdateformat="^(20|19)([0-9][0-9])/(0[1-9]|1[0-2])/(0[1-9]|1[0-9]|2[0-9]|3[0-1])$"
+  [[ $selecteddate =~ $correctdateformat ]] \
+  &&
+  { wordoftheday=`curl -s -L "www.dictionary.com/wordoftheday/$selecteddate" \
+  | grep "<title>" | sed 's/<title>//g' | cut -d "-" -f 1 | sed 's: | \
+  Dictionary.com</title>::g' | tr  -d ' '` ;} \
+  || { echo "Bad date format - please use YYYY/MM/DD" ; exit; }
+
+  cleandate=`echo $selecteddate | sed 's|/||g'`
+  if [ "`date '+%Y%m%d'`" -lt "$cleandate" ]; then
+    echo "Date is in the future! Please select a valid date." ; exit;
+  fi
+fi
 
 [ -z "$selecteddate" ] && selecteddate=`date +%Y/%m/%d` && wordoftheday=`curl \
 -s -L "www.dictionary.com/wordoftheday/$selecteddate" | grep "<title>" | \
