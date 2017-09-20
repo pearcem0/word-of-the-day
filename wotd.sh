6cd02bcd7d9e690b6bfb2243c08a2cabc982d40f
Michael Pearce <michael@rootofpi.co.uk>
2017-09-20 12:46:09 +0100

    added date format check

--- wotd.sh
+++ wotd.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
+function exitscript {
+  exit 1;
+}
 
 while getopts ":d:" opt; do
   case $opt in
     d)
       selecteddate=$OPTARG
-      wordoftheday=`curl -s -L "www.dictionary.com/wordoftheday/$selecteddate" \
-      | grep "<title>" | sed 's/<title>//g' | cut -d "-" -f 1 | sed 's: | \
-      Dictionary.com</title>::g' | tr  -d ' '`
       ;;
   esac
 done
+
+if [ "$selecteddate" ];
+then
+  correctdateformat="^(20|19)([0-9][0-9])/(0[1-9]|1[0-2])/(0[1-9]|1[0-9]|2[0-9]|3[0-1])$"
+  [[ $selecteddate =~ $correctdateformat ]] \
+  &&
+  { wordoftheday=`curl -s -L "www.dictionary.com/wordoftheday/$selecteddate" \
+  | grep "<title>" | sed 's/<title>//g' | cut -d "-" -f 1 | sed 's: | \
+  Dictionary.com</title>::g' | tr  -d ' '` ;} \
+  || { echo "Bad date format - please use YYYY/MM/DD" ; exit; }
+fi
 
 [ -z "$selecteddate" ] && selecteddate=`date +%Y/%m/%d` && wordoftheday=`curl \
 -s -L "www.dictionary.com/wordoftheday/$selecteddate" | grep "<title>" | \
